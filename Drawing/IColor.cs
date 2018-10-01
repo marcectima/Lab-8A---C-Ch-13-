c7c4937378295806601513655fe38629305991d6
Marckenly Estima <timhoward5@yahoo.com>
2018-09-30 23:52:19 -0400

    Add project files.

---
+++ Drawing/IColor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    interface IColor
+    {
+        void IColor.SetColor(Color color)
+        {
+            if (this.rect != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.rect.Fill = brush;
+            }
+        }
+    }
+}
---
+++ Drawing/IDraw.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+namespace Drawing
+{
+    interface IDraw
+    {
+      void SetLocation(int xCoord, int yCoord);
+      void Draw(Canvas canvas);
+    }
+}
